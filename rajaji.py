6c7aea088407bef04df9aabd651787aab812a8d0
tintin400819 <tintin.400819@gmail.com>
2025-02-12 22:55:22 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,11 @@
+# Don't use quotes(  and  )
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+
+# Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7688926856:AAGlX97yp1K8omBgRVkIUe_r5KoY1T7_RzU")
+
+# Enter Your telegram usernames here without @
+OWNER_USERNAME=("@Ayush143", "@ClutchGodOp")
+
+# Enter your admin IDs here. Get it from @missRose_bot by typing /info
+ADMIN_IDS=("859818729", "787517301")
